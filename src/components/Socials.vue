56cece46a3884cda28b3adec14076b7167cb1aa4
吳元皓 <hpware3@gmail.com>
2024-06-16 16:52:15 +0800

    socials.vue update

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -2,7 +2,9 @@
 <template>
     <div>
         <h1>吳元皓 </h1>
-        <h2>hpware 的 Vue 版</h2>
+        <h2>hpware.hwtw.cc</h2>
+        <p>我的 Vue 版網站
+        <br>可以在 GitHub 上找到 Source Code</p>
         <br>
     </div>
 </template>
