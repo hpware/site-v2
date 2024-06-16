065a62b53388a09de3a6bcdda20f9c5999bebf56
吳元皓 <hpware3@gmail.com>
2024-06-16 17:44:57 +0800

    vercel no fee

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ else if (golink == "res") {window.location.href = "https://github.com/hpware/sit
 
 <template>
   <header>
-    <img alt="hpware" class="logo" style="border-radius: 50%;" src="./assets/pfp2.png" />
+    <img alt="hpware" class="logo" style="border-radius: 50%;" src="https://storage.hpware.hwtw.cc/img/index-pfp.png" />
     <div class="wrapper">
       <Socials />
     </div>
