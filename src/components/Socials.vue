edb0310849398e71a6acd9998adf16dc3c4c88b4
吳元皓 <hpware3@gmail.com>
2024-06-16 20:01:31 +0800

    getting creative

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -3,9 +3,9 @@ import Gitstats from './Gitstats.vue'
 </script>
 <template>
     <div>
-        <h1>吳元皓 </h1>
-        <h2>hpware.hwtw.cc</h2>
-        <p>我的 Vue 版網站
+        <h1>name: {吳元皓}</h1>
+        <h2>url: {hpware.hwtw.cc}</h2>
+        <p>config: {討厭 white mode}
         <br>GitHub 的 Stats</p>
         <Gitstats />
     </div>
