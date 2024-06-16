b5845b34dfbec02caa5549227ad49ede4e2e30f1
吳元皓 <hpware3@gmail.com>
2024-06-16 09:36:01 +0800

    ver

--- src/components/SideStuff.vue
+++ src/components/SideStuff.vue
@@ -20,7 +20,7 @@
             </li>
             <li>Site-V1 (<a style="color:aquamarine" href="https://github.com/hpware/hpware.github.io">Source Code</a>)(<a href="https://3002.hwtw.cc">網站</a>)</li>
             <li>Site-V2 (<a style="color:aquamarine" href="https://github.com/hpware/hpware-site-v2">Source Code</a>)(<a href="https://hpware.hwtw.cc">網站</a>)</li>
-            <li>lovemusic.asia (<a style="color:aquamarine" href="/sourcecodena">Source Code</a>)(<a href="https://www.lovemusic.asia">網站</a>)</li>
+            <li>lovemusic.asia (<a style="color:aquamarine" href="/sourcecodena.html">Source Code</a>)(<a href="https://www.lovemusic.asia">網站</a>)</li>
         </ul>
         <br>
         <hr>
