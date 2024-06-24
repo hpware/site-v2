be686ee24ea2e605a0eb97af70c7e6a4af4374bd
吳元皓 <hpware3@gmail.com>
2024-06-24 21:52:41 +0800

    last

--- src/components/Remain-ig-info1.vue
+++ src/components/Remain-ig-info1.vue
@@ -2,24 +2,22 @@
 import Link from './Link.vue'
 </script>
 <template>
-    <section>
         <h1>Remain-IG-info <a href='?core=remain-ig-info#40fci'><Link/></a></h1>
------BEGIN PGP SIGNED MESSAGE-----
-Hash: SHA512
-
-我在幾乎所有的社群媒體上都是朋(盆)友 if you realy care about me than why the hell don't you @ me? Don't ask me. Ask yourself. If you don't care about me, than I should be the same person (which is maybe you), why should I treat the same person this makes me looks like shit.
-Put this thing to a few words that you will (maybe) be able to understand. 
-If your the person that I'm targeting, then you should rethink your actions. 
-Treat me like air? I'm going to treat you as air as well.
------BEGIN PGP SIGNATURE-----
-
-iHUEARYKAB0WIQRUCVvCkNTahUBWcFG6y37R0nwgSQUCZnl30QAKCRC6y37R0nwg
-Sae8AQCpJgyOsqRE8sEVbBuVJFkvP6kCiLUhb3MQggEhiD/rOAD+NaL4A5lTncu6
-00pLV6/7JZWNPhx2DFExs50UXqsJJAg=
-=LwYF
+-----BEGIN PGP SIGNED MESSAGE-----<br>
+Hash: SHA512<br>
+<br>
+我在幾乎所有的社群媒體上都是朋(盆)友 if you realy care about me than why the hell don't you @ me? Don't ask me. Ask yourself. If you don't care about me, than I should be the same person (which is maybe you), why should I treat the same person this makes me looks like shit.<br>
+Put this thing to a few words that you will (maybe) be able to understand. <br>
+If your the person that I'm targeting, then you should rethink your actions. <br>
+Treat me like air? I'm going to treat you as air as well.<br>
+<br>
+-----BEGIN PGP SIGNATURE-----<br>
+iHUEARYKAB0WIQRUCVvCkNTahUBWcFG6y37R0nwgSQUCZnl30QAKCRC6y37R0nwg<br>
+Sae8AQCpJgyOsqRE8sEVbBuVJFkvP6kCiLUhb3MQggEhiD/rOAD+NaL4A5lTncu6<br>
+00pLV6/7JZWNPhx2DFExs50UXqsJJAg=<br>
+=LwYF<br>
 -----END PGP SIGNATURE-----
 
         <br>
         <hr>
-        </section>
 </template>
